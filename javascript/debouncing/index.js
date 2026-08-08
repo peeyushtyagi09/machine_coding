e42b869b781ee9d5b5fec124bc464987174f396f
peeyushtyagi09 <peeyushtyagi82@gmail.com>
2026-08-08 15:52:44 +0530

    filter bhi hogaya data

--- javascript/debouncing/index.js
+++ javascript/debouncing/index.js
@@ -1,10 +1,17 @@
-const data = async() => {
-    try{
+
+const fetchData = async () => {
+    try {
         const response = await fetch('https://dummyjson.com/products?sortBy=title&order=asc');
         return response.json();
-    }catch(err){
-        console.log(' Their is some error in fetching data from server ->', err);
+    } catch (err) {
+        console.log('There is some error in fetching data from server ->', err);
     }
-}
+};
 
-data().then(console.log);
+fetchData().then(result => {
+    if (result && result.products && result.products.length > 0) { 
+        result.products.forEach(product => {
+            console.log(product.category);
+        });
+    }
+});
