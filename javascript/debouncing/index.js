933827a8d9e7d04cd5446edd29893fd91e97dc2d
peeyushtyagi09 <peeyushtyagi82@gmail.com>
2026-08-08 15:33:17 +0530

    .

--- javascript/debouncing/index.js
+++ javascript/debouncing/index.js
@@ -6,13 +6,3 @@ const data = async () => {
         console.log('There is some error in fetching data ->', err);
     }
 };
-
-data().then(result => {
-    if (result && result.products && result.products.length > 0) {
-        result.products.forEach(product => {
-            console.log(product.category);
-        });
-    } else {
-        console.log('No products found or invalid result.');
-    }
-});
