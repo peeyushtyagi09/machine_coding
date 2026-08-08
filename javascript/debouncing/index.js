3fe9ec79790d83b247a0793416fdf0b35ae3627f
peeyushtyagi09 <peeyushtyagi82@gmail.com>
2026-08-08 16:48:40 +0530

    okk. fata render bhi hoga

--- javascript/debouncing/index.js
+++ javascript/debouncing/index.js
@@ -1,17 +1,74 @@
+const API_URL = "https://dummyjson.com/products?limit=100";
 
-const fetchData = async () => {
-    try {
-        const response = await fetch('https://dummyjson.com/products?sortBy=title&order=asc');
-        return response.json();
-    } catch (err) {
-        console.log('There is some error in fetching data from server ->', err);
-    }
-};
+const productList = document.getElementById("productList");
+const loading = document.getElementById("loading");
+const error = document.getElementById("error");
 
-fetchData().then(result => {
-    if (result && result.products && result.products.length > 0) { 
-        result.products.forEach(product => {
-            console.log(product.category);
-        });
+async function fetchProducts() {
+    try{
+        loading.style.display = "block";
+        error.textContent = "";
+
+        const response = await fetch(API_URL);
+
+        if(!response.ok){
+            throw new Error("failed to fetch products");
+        }
+        const data = await response.json();
+
+        return data.products;
+    }catch (err){
+        error.textContent = err.message;
+        return [];
+    }finally {
+        loading.style.display = "none";
     }
-});
+}
+
+function renderProducts(products){
+    productList.innerHTML = "";
+    products.forEach((product) => {
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+        <td>${product.id}</td>
+
+        <td>${product.title}</td>
+
+        <td>${product.category}</td>
+
+        <td>$${product.price}</td>
+
+        <td>
+            <img
+                src="${product.thumbnail}"
+                alt="${product.title}"
+            >
+        </td>
+    `;
+     row.innerHTML = `
+            <td>${product.id}</td>
+
+            <td>${product.title}</td>
+
+            <td>${product.category}</td>
+
+            <td>$${product.price}</td>
+
+            <td>
+                <img
+                    src="${product.thumbnail}"
+                    alt="${product.title}"
+                >
+            </td>
+        `;
+        productList.appendChild(row);
+    });
+}
+
+async function init(){
+    const products = await fetchProducts();
+    renderProducts(products);
+}
+
+init();
