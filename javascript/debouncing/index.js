7e1070d15a5a958ff30486bad9d7cc97a292378d
peeyushtyagi09 <peeyushtyagi82@gmail.com>
2026-08-08 15:44:36 +0530

    data fetch ho gaya

--- javascript/debouncing/index.js
+++ javascript/debouncing/index.js
@@ -1,8 +1,10 @@
-const data = async () => {
-    try {
-        const response = await fetch('https://dummyjson.com/products/search?q=phone');
-        return await response.json();
-    } catch (err) {
-        console.log('There is some error in fetching data ->', err);
+const data = async() => {
+    try{
+        const response = await fetch('https://dummyjson.com/products?sortBy=title&order=asc');
+        return response.json();
+    }catch(err){
+        console.log(' Their is some error in fetching data from server ->', err);
     }
-};
+}
+
+data().then(console.log);
