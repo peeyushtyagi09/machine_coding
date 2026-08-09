e46fcdf797cc5e5e11e291a95d2b743ded214fe8
peeyushtyagi09 <peeyushtyagi82@gmail.com>
2026-08-09 11:23:45 +0530

    Hum yeh soch kar koi vaada karo, ki vaade pe umrein guzar jayengi.

--- javascript/scenario1/index.js
+++ javascript/scenario1/index.js
@@ -5,20 +5,71 @@ const loading = document.getElementById("loading");
 const error = document.getElementById("error");
 
 let products = [];
-const fetchData = async() => {
+
+async function fetchData(){
     try{
+        loading.style.display = "block";
+        error.textContent = "";
+
         const response = await fetch(API_URL);
-        return response.json();
+
+        if(!response.ok){
+            throw new Error("Failed[ to fetch products");
+        }
+        
+        const data = await response.json();
+        return data.products;
     }catch(err){
-        console.log("their are some error in that", err);
+        error.textContent = err.message;
+        return [];
+    }finally {
+        loading.style.display = "none";
     }
 }
 
 // fetchData().then(console.log);
 function renderData(products){
-    
+    productList.innerHTML = "";
+    if (products.length === 0){
+        productList.innerHTML = `
+            <tr>
+                <td colspan="5">
+                    No products found
+                </td>
+            </tr>
+        `;
+        return;
+    }
+
+    products.forEach((product) => { 
+        const card = document.createElement("div");
+        card.className = "product-card";
+
+        card.innerHTML = `
+            <img
+                class="product-img"
+                src="${product.thumbnail}"
+                alt="${product.title}"
+            >
+            <div class="product-title">${product.title}</div>
+            <div class="product-category">${product.category}</div>
+            <div class="product-price">$${product.price}</div>
+            <button class="add-btn">Add</button>
+        `;
+
+        const addButton = card.querySelector(".add-btn");
+        addButton.addEventListener("click", () => {
+            handleAddToCart(product);
+        });
+
+        productList.appendChild(card);
+    });
 };
 
+
+async function handleAddToCart(){
+    console.log("ye kaam kar raha h...");
+}
 async function helper() { 
     products = await fetchData(); 
     renderData(products);
