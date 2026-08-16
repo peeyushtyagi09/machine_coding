cc88d82af6c8cd7f968616ecf65db3ab2034adbd
peeyushtyagi09 <peeyushtyagi82@gmail.com>
2026-08-16 16:10:22 +0530

    Those days...

--- react/Question/src/Questions/Question1/Question1.jsx
+++ react/Question/src/Questions/Question1/Question1.jsx
@@ -1,7 +1,15 @@
+import employees from "./data.js";
+
+let array = employees;
 function Question1(){
+    console.log(array);
+    array.forEach((e)=>{
+        console.log(e.id);
+    })
     return (
         <div>
             <h1>Question 1</h1>
+            
         </div>
     )
 }
